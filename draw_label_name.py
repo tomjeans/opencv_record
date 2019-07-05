ce94e77bad275d2db1be5109da6f3cb088fb6298
tomjeans <37204571+tomjeans@users.noreply.github.com>
2019-07-05 16:58:18 +0800

    Add files via upload
    the function adding to the future

--- draw_label_name.py
+++ draw_label_name.py
@@ -4,8 +4,6 @@ Created on Thu Jul  4 14:52:42 2019
 
 @author: Xiepeng
 """
-list_positons_one= [[22,33],[11,44],[23,10],[33,21],[22,33]]
-list_position_second = [[11,33],[15,55],[18,60],[24,77],[30,68],[42,72],[50,61],[10,-20],[20,-30]]
 #like (x,y)
 #processing the dict_x:y {x_n:y_n} dict_y:x {y_n:x_n}
 #first find the medium_number {x_medium:y_n} {y_medium:x_n}
@@ -22,8 +20,10 @@ positions_y=[]
 dict_x={}
 dict_y={}
 info_distance={}
+info_label_name={}
 import cv2
 import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 im=cv2.imread('C:\\Users\\LKJ\\Desktop\\lian_2.jpg')
 w,h,n= im.shape
 im_gray = cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
@@ -31,7 +31,8 @@ _,thresh = cv2.threshold(im_gray,128,255,cv2.THRESH_BINARY)
 
 nccomps = cv2.connectedComponentsWithStats(thresh)#labels,stats,centroids
 centroids = nccomps[3]# center position x,y [[x1,y1],[x2,y2],...[xn,yn]]
-cv2.imshow('th',thresh)
+#cv2.imshow('th',thresh)
+#solve the positions
 for center in centroids:
     ce = (int(center[0]),int(center[1]))
     positions_x.append(int(center[0]))
@@ -62,6 +63,72 @@ for positions_x_sort_s in positions_x_sort:
     info_distance[(positions_x_sort_s,dict_x[positions_x_sort_s])]=(distance_x,distance_y)
 for position_line_start in info_distance:
     cv2.line(im,position_line_start, tuple(np.array(position_line_start)+np.array(info_distance[position_line_start])),(0,0,255),1)
-cv2.imshow('im_',im)
+    label_name_start_position = tuple(np.array(position_line_start)+np.array(info_distance[position_line_start]))
+    info_label_name[label_name_start_position] = 'test(注意字体使用)'#label_name
+    #cv2.putText(im, 'test', label_name_start_position, cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+#info_distance[position_line_start] is (distance_x,distance_y)
+#position_line_start is the start point 
+im_pil=Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+font = ImageFont.truetype('Alibaba-PuHuiTi-Regular.otf',22)
+for position_label_name in info_label_name:
+    label_str = info_label_name[position_label_name]
+    # str = str.encode('utf8')
+    draw = ImageDraw.Draw(im_pil)
+    draw.text(position_label_name, label_str, font=font, fill=(255, 0, 0))
+im_show = cv2.cvtColor(np.asarray(im_pil),cv2.COLOR_RGB2BGR)
+cv2.imshow('im_',im_show)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+#########################################################
+def draw_labe_name_(positions,positions_x,positions_y,dic_info_label_name):
+    import cv2
+    import numpy as np
+    from PIL import Image, ImageDraw, ImageFont
+    '''dic_info_label_name:{(center_x,center_y):label_name_kind}
+       positions:[(x1,y1),(x2,y2),..(xn,yn)]
+       positions_x:[x1,x3,x2,xn]
+       position_y:[y1,y3,y2,yn]
+    '''
+    positions=[]
+    positions_x=[]
+    positions_y=[]
+    dict_x={}
+    dict_y={}
+    info_distance={}
+    info_label_name={}
+    for positions_single in positions:
+        dict_x[positions_single[0]]=positions_single[1]
+        dict_y[positions_single[1]]=positions_single[0]
+    positions_x_sort=sorted(positions_x)
+    positions_y_sort=sorted(positions_y)
+    for positions_x_sort_s in positions_x_sort:
+        if dict_x[positions_x_sort_s] < positions_y_sort[int(len(positions_y_sort)/2)]:
+            distance_y = -88
+        elif dict_x[positions_x_sort_s] > positions_y_sort[int(len(positions_y_sort)/2)]:
+            distance_y = 88
+        else:
+            distance_y = 0
+        if positions_x_sort_s < positions_x_sort[int(len(positions_x_sort)/2)]:
+            distance_x = -20
+        elif positions_x_sort_s > positions_x_sort[int(len(positions_x_sort)/2)]:
+            distance_x = 20
+        else:
+            distance_x = 0
+        info_distance[(positions_x_sort_s,dict_x[positions_x_sort_s])]=(distance_x,distance_y)
+    for position_line_start in info_distance:
+        cv2.line(im,position_line_start, tuple(np.array(position_line_start)+np.array(info_distance[position_line_start])),(0,0,255),1)
+        label_name_start_position = tuple(np.array(position_line_start)+np.array(info_distance[position_line_start]))
+        label_name_pp[position_line_start] = label_name_start_position
+        info_label_name[label_name_start_position] = dic_info_label_name[position_line_start]#dict_info_label_name (position_x,position_y):label_name
+        #cv2.putText(im, 'test', label_name_start_position, cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        #info_distance[position_line_start] is (distance_x,distance_y)
+        #position_line_start is the start point 
+    im_pil=Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+    font = ImageFont.truetype('Alibaba-PuHuiTi-Regular.otf',22)
+    for position_label_name in info_label_name:
+        label_str = info_label_name[position_label_name]
+        # str = str.encode('utf8')
+        draw = ImageDraw.Draw(im_pil)
+        draw.text(position_label_name, label_str, font=font, fill=(255, 0, 0))
+        im_show = cv2.cvtColor(np.asarray(im_pil),cv2.COLOR_RGB2BGR)
+    return im_show
